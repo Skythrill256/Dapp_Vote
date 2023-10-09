36b9626173cc6e02b20ee959ad0b7b34771a6edc
Anjishnu Ganguly <113181374+Skythrill256@users.noreply.github.com>
2023-10-09 23:00:26 +0530

    inserted the abi in separate folder

--- context/constant.js
+++ context/constant.js
@@ -1,1 +1,5 @@
 //abi of the contract
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+import voting from './votingContract.json'
+export const VotingAddress = 0x5FbDB2315678afecb367f032d93F642f64180aa3
+export const VottingAddress = voting.abi;
